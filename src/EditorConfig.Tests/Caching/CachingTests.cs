fc527b3da6853f129f60dba69625403be1b09a8f
nojaf <florian.verdonck@outlook.com>
2023-03-01 14:51:25 +0100

    Add factory overload to EditorConfigParser.

---
+++ src/EditorConfig.Tests/Caching/CachingTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using EditorConfig.Core;
+using NUnit.Framework;
+
+namespace EditorConfig.Tests.Caching
+{
+	[TestFixture]
+	public class CachingTests : EditorConfigTestBase
+	{
+		[Test]
+		public void FileShouldCached()
+		{
+			var fileName = GetFileFromMethod(MethodBase.GetCurrentMethod(),  ".editorconfig");
+			var parser = new EditorConfigParser(EditorConfigFileCache.GetOrCreate);
+			var config1 = parser.Parse(fileName);
+			var config2 = parser.Parse(fileName);
+			// Not sure how to assert this...
+		}
+	}
+}
