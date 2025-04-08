b1cec6ab3c8e731bd1b5e4f979ea446ecf4ba59b
huangqinjin <huangqinjin@gmail.com>
2025-04-09 00:18:04 +0800

    Allow reading config files at filesystem/drive root

--- src/EditorConfig.Core/EditorConfigParser.cs
+++ src/EditorConfig.Core/EditorConfigParser.cs
@@ -134,7 +134,6 @@ namespace EditorConfig.Core
 
 		private IEnumerable<string> AllParentDirectories(string fullPath)
 		{
-			var root = new DirectoryInfo(fullPath).Root.FullName;
 			var dir = Path.GetDirectoryName(fullPath);
 			do
 			{
@@ -143,7 +142,7 @@ namespace EditorConfig.Core
 				var dirInfo = new DirectoryInfo(dir);
 				if (dirInfo.Parent == null) yield break;
 				dir = dirInfo.Parent.FullName;
-			} while (dir != root);
+			} while (true);
 		}
 	}
 }
